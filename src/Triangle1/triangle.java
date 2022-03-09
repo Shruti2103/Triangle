d1de29d640b22b1f0e1f419df848713b3b0a8f83
Shruti2103 <shurtidevani21@gmail.com>
2022-03-09 22:55:17 +0000

    Triangle

--- src/Triangle1/triangle.java
+++ src/Triangle1/triangle.java
@@ -8,6 +8,16 @@ public class triangle {
         System.out.println("   ***"   );
         System.out.println("    *    ");
 
+       int a=10;
+       int b=20;
+
+
+       if (a>b){
+           System.out.println("b is biggest");
+       }
+     else {
+         System.out.println("a is biger");
+       }
 
 
     }
